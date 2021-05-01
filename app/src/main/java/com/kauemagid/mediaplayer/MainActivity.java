9f2c486c80cfe7cbcb3cf47731858d37477c0223
Kaue Salem <magidsalem96@gmail.com>
2021-05-01 20:54:56 -0300

    Volume ajust in SneekBar and onDestroy method

--- app/src/main/java/com/kauemagid/mediaplayer/MainActivity.java
+++ app/src/main/java/com/kauemagid/mediaplayer/MainActivity.java
@@ -2,19 +2,59 @@ package com.kauemagid.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bach);
+        initSeekBar();
+    }
+
+    private void initSeekBar(){
+        seekVolume = findViewById(R.id.seekBarVolume);
+
+        //config Audio Mananger
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(audioManager.STREAM_MUSIC);
+
+        //Config max values for seekBar
+        seekVolume.setMax(maxVolume);
+
+        //Config progress seekBar
+        seekVolume.setProgress(currentVolume);
+
+        //Config user slide
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void playSong(View v){
@@ -36,4 +76,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
