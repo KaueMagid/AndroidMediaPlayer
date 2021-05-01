b9a4a99c68f06fa3c8393491db7f090afae36a52
Kaue Salem <magidsalem96@gmail.com>
2021-05-01 19:52:29 -0300

    Layout page, play, pause and stop methods

--- app/src/main/java/com/kauemagid/mediaplayer/MainActivity.java
+++ app/src/main/java/com/kauemagid/mediaplayer/MainActivity.java
@@ -2,13 +2,38 @@ package com.kauemagid.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.bach);
     }
+
+    public void playSong(View v){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+
+    public void pauseSong(View v){
+        if (mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stopSong(View v){
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.noite_de_terror);
+        }
+    }
+
 }
